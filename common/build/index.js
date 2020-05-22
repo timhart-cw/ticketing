e9e036f82a3d772ee4594dafca2e846958a18a12
Tim Hart <timhart@Tim-Hart-MacBook-Pro.local>
2020-05-22 16:52:22 +0100

    starting section 13

--- common/build/index.js
+++ common/build/index.js
@@ -10,6 +10,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+//re export stuff from errors and middlewares
 __exportStar(require("./errors/bad-request-error"), exports);
 __exportStar(require("./errors/custom-error"), exports);
 __exportStar(require("./errors/database-connection-error"), exports);
