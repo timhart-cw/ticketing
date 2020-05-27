3ef9b32240962116497b068537106b06b35e70d6
Tim Hart <timhart@Tim-Hart-MacBook-Pro.local>
2020-05-27 11:41:20 +0100

    starting section 18

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+//making sure we export this so that it's available as reference in Order.ts
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  //put method signature in here
+  isReserved(): Promise<boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+//this is how we actually add the method
+ticketSchema.methods.isReserved = async function () {
+  //this === the ticket document implementation
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+  //this means it will return true or false - even if it's null
+  return !!existingOrder;
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+export { Ticket };
---
+++ orders/src/routes/delete.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorisedError,
+} from '@cwticketing/common';
+import { Order, OrderStatus } from '../models/order';
+
+const router = express.Router();
+
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorisedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
+
+export { router as deleteOrderRouter };
