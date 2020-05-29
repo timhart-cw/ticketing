c694b03084ff65fb4e89161ced3f7324fa11126b
Tim Hart <timhart@Tim-Hart-MacBook-Pro.local>
2020-05-29 12:51:18 +0100

    starting section 21

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,24 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@cwticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('waiting milliseconds ', delay);
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
+
+    msg.ack();
+  }
+}
---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,45 @@
+import {
+  Subjects,
+  Listener,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from '@cwticketing/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from '../listeners/queue-group-name';
+import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+export class ExpirationCompleteListener extends Listener<
+  ExpirationCompleteEvent
+> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    //we have to chain the .populate on the below statement to make sure we get the corresponding ticket
+    const order = await Order.findById(data.orderId).populate('ticket');
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+        price: order.ticket.price,
+      },
+    });
+
+    msg.ack();
+  }
+}
