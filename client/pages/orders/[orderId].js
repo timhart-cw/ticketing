0d46eba34cb43acbbce1b90da00969ace581b213
Tim Hart <timhart@Tim-Hart-MacBook-Pro.local>
2020-06-01 12:35:51 +0100

    finished section 22

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => Router.push('/orders'),
+  });
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    //this will be called when navigating away from the component.  The component will only be rerendered if there
+    //is something inside of the dependency array (second param of useEffect - which we have as [])
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_AOrEf6O9m2VP9VeEleo5JLBJ00wTIWvTac"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  //get the orderId from the query string
+  const { orderId } = context.query;
+  //get order data from api
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data }; // this can then be destructured in the OrderShow init above
+};
+export default OrderShow;
